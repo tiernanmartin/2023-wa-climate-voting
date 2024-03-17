fcc27a5f995f7e2a2d8722fc40bdfb47db2c68ea
Tiernan Martin <tiernan.martin@gmail.com>
2024-03-16 20:58:22 -0700

    set up target pipeline

---
+++ _targets.R
@@ -0,0 +1,24 @@
+
+# LOAD LIBRARIES ----------------------------------------------------------
+
+library(targets)
+library(tidyverse)
+
+
+# SET OPTIONS -------------------------------------------------------------
+
+tar_option_set(
+  packages = c("tidyverse")
+)
+
+
+# LOAD TARGET FUNCTIONS ---------------------------------------------------
+
+tar_source()
+
+
+# TARGET PIPELINE ---------------------------------------------------------
+
+list(
+
+)
