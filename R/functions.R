56485228c51e6ad285b42141aa63bd369697f381
Tiernan Martin <tiernan.martin@gmail.com>
2024-04-15 08:39:12 -0700

    separate the models into three progressive versions: simple, extended, spatial

--- R/functions.R
+++ R/functions.R
@@ -1,13 +1,13 @@
 
 # TARGET FUNCTIONS --------------------------------------------------------
 
-make_model_spatial_lag <- function(model_data, model_lm, model_spatial_weights){
+make_model_spatial_lag <- function(model_data, model_lm_extended, model_spatial_weights){
   
   model_data <- model_data |> 
     drop_na()
   
   model_spatial_lag <- lagsarlm(
-    formula = model_lm, 
+    formula = model_lm_extended, 
     data = model_data, 
     listw = model_spatial_weights,
     zero.policy = TRUE
@@ -28,15 +28,26 @@ make_model_spatial_weights <- function(model_data){
   return(model_spatial_weights)
 }
 
-make_model_lm <- function(model_data){
+make_model_lm_extended <- function(model_data){
   
   model_data <- model_data |> 
     drop_na()
   
-  model_lm <- lm(vote_i0732n_pct ~ hh_vmt + vote_rep_pct, 
+  model_lm_extended <- lm(vote_i0732n_pct ~ hh_vmt + vote_rep_pct, 
                  data = model_data)
   
-  return(model_lm)
+  return(model_lm_extended)
+}
+
+make_model_lm_simple <- function(model_data){
+  
+  model_data <- model_data |> 
+    drop_na()
+  
+  model_lm_simple <- lm(vote_i0732n_pct ~ hh_vmt, 
+                          data = model_data)
+  
+  return(model_lm_simple)
 }
 
 make_model_data <- function(hh_vmt_2012_2016, tracts_vote_2016){
@@ -80,8 +91,8 @@ make_tracts_vote_2016 <- function(wa_blocks,
     crs = 6152) |> 
     rowwise() |> 
     mutate(
-      vote_rep_pct = g16prstrum / sum(c_across(starts_with("g16prs"))),
-      vote_i0732n_pct = g16i0732n / sum(g16i0732n, g16i0732y)
+      vote_rep_pct = g16prstrum / sum(c_across(starts_with("g16prs")), na.rm = TRUE),
+      vote_i0732n_pct = g16i0732n / sum(g16i0732n, g16i0732y, na.rm = TRUE)
     )
     
   
--- _targets.R
+++ _targets.R
@@ -82,11 +82,13 @@ pipeline_data <- list(
 
 pipeline_models <- list(
   tar_target(model_spatial_lag,
-             make_model_spatial_lag(model_data, model_lm, model_spatial_weights)),
+             make_model_spatial_lag(model_data, model_lm_extended, model_spatial_weights)),
   tar_target(model_spatial_weights,
              make_model_spatial_weights(model_data)),
-  tar_target(model_lm,
-             make_model_lm(model_data))
+  tar_target(model_lm_extended,
+             make_model_lm_extended(model_data)),
+  tar_target(model_lm_simple,
+             make_model_lm_simple(model_data))
 )
 
 # PROJECT PIPELINE --------------------------------------------------------
